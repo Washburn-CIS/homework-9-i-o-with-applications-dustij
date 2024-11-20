8ef473ef3b8a53b5194164da5024e9b434d91472
dustij <knightlybyte@gmail.com>
2024-11-20 11:33:14 -0600

    add new student feature

--- gradebook/GradeBook.java
+++ gradebook/GradeBook.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -16,12 +15,12 @@ public class GradeBook {
         try (BufferedReader br = new BufferedReader(new FileReader("grades.txt"))) {
             int numberOfStudentsInFile = Integer.parseInt(br.readLine());
             students = new Student[numberOfStudentsInFile];
-            
+
             String studentData;
             int i = 0;
             while ((studentData = br.readLine()) != null) {
                 String[] data = studentData.split(",");
-                
+
                 String firstName = data[0];
                 String lastName = data[1];
                 try {
@@ -29,7 +28,8 @@ public class GradeBook {
                     students[i] = new Student(firstName, lastName, grade);
                     i++;
                 } catch (NumberFormatException e) {
-                    System.out.println("Error parsing student grade from 'grades.txt' -> " + firstName + " " + lastName);
+                    System.out.println("Error parsing student grade from 'grades.txt' -> "
+                            + firstName + " " + lastName);
                     System.exit(1);
                 }
 
@@ -41,40 +41,41 @@ public class GradeBook {
 
         System.out.println("Welcome to the CM111 Grade Book App!");
 
-        while(true) {
+        while (true) {
             System.out.println("\nPlease make a selection:\n");
             System.out.println("1) List Class Grades");
             System.out.println("2) Update Grade");
             System.out.println("3) Calculate Exam Average");
-            System.out.println("4) Exit");
+            System.out.println("4) Add New Student");
+            System.out.println("5) Exit");
             System.out.print("\nPlease choose an option: ");
             String choice = input.nextLine();
             System.out.println();
-            switch(choice) {
-                case "1": 
-                    for(Student student: students) {
-                        System.out.printf("%s, %s: %f%n", student.getLastName(), 
-                                                        student.getFirstName(), 
-                                                        student.getGrade());
+            switch (choice) {
+                case "1":
+                    for (Student student : students) {
+                        System.out.printf("%s, %s: %f%n", student.getLastName(),
+                                student.getFirstName(), student.getGrade());
                     }
                     break;
-                case "2":
+                case "2": {
                     System.out.println("Enter First Name: ");
                     String fname = input.nextLine();
                     System.out.println("Enter Last Name: ");
                     String lname = input.nextLine();
-                    
-                    for(Student student: students) {
-                        if(student.getFirstName().equals(fname) &&
-                           student.getLastName().equals(lname)) {
-                           System.out.println("Enter Grade: ");
-                           student.setGrade(Double.parseDouble(input.nextLine()));
-                           System.out.println("Grade updated");
-                           continue;
+
+                    for (Student student : students) {
+                        if (student.getFirstName().equals(fname)
+                                && student.getLastName().equals(lname)) {
+                            System.out.println("Enter Grade: ");
+                            student.setGrade(Double.parseDouble(input.nextLine()));
+                            System.out.println("Grade updated");
+                            continue;
                         }
                     }
                     System.out.println("Student not found");
                     break;
+                }
                 case "3":
                     System.out.print("Enter grades separated by spaces: ");
                     String[] strGrades = input.nextLine().split(" ");
@@ -85,12 +86,37 @@ public class GradeBook {
                     int avgGrade = Student.getExamAverage(intGrades);
                     System.out.println("Exam average grade is: " + avgGrade);
                     break;
-                case "4":
+                case "4": {
+                    System.out.println("Enter First Name: ");
+                    String fname = input.nextLine();
+                    System.out.println("Enter Last Name: ");
+                    String lname = input.nextLine();
+                    System.out.println("Enter a grade: ");
+                    try {
+                        double grade = Double.parseDouble(input.nextLine());
+                        Student[] copyStudents = new Student[students.length + 1];
+                        for (int i = 0; i < copyStudents.length; i++) {
+                            // copy students over, adding new student to end
+                            if (i < copyStudents.length - 1) {
+                                copyStudents[i] = students[i];
+                            } else {
+                                copyStudents[i] = new Student(fname, lname, grade);
+                            }
+                        }
+                        // set this students attribute to new array
+                        students = copyStudents;
+                    } catch (NumberFormatException e) {
+                        System.out.println("Please enter a number for the grade.");
+                    }
+                    break;
+                }
+                case "5":
                     // Challenge: write code to save the grades to grades.txt
                     try (PrintWriter writer = new PrintWriter(new File("grades.txt"))) {
                         writer.println(students.length);
                         for (Student student : students) {
-                            writer.printf("%s,%s,%.0f%n", student.getFirstName(), student.getLastName(), student.getGrade());
+                            writer.printf("%s,%s,%.0f%n", student.getFirstName(),
+                                    student.getLastName(), student.getGrade());
                         }
                     } catch (IOException e) {
                         System.out.println("Failed to save updates to 'grades.txt'");
